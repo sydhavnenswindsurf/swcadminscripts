608b57390096a4d25ce7c604cf524e908015e2d9
Rasmus Lohals <rll@schultz.dk>
2018-03-10 00:46:00 +0100

    fixed typeings stuff

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="medlemmercommon/medlemmer_common_lib.d.ts" />
-/// <reference path="swcadmincommon/swcadmin_common_lib.d.ts" />
-
--- medlemmercommon/medlemmer_common_lib.d.ts
+++ medlemmercommon/medlemmer_common_lib.d.ts
@@ -1,19 +1,17 @@
-declare module 'medlemmer_common'{
-     
-    export interface medlemmer_common{
-        INDMELDELSESDATO_COLUMNID:number;
-        UDMELDT_COLUMN_ID:number;
-        UDMELDELSES_COLUMNHEADER:string;
-        getMedlemmerStamdata(): any;
-        getMedlemmerSheet(): any;
-        update(updateMedlem: any): void;
-        addStamdataForMedlem(email: any, stamDataForMedlem: any): void;
-        getNytMedlemsnummer(): any;
-        erUdmeldt(email: any, stamdata: any): boolean;
-        erMedlem(email: any, stamdata: any): boolean;
-        getMedlemsNummmer(email: any, stamdata: any): any;
-        harHylde(email: any): boolean;
-        setUdmeldtStatus(email: any): void;
-        erUdmeldt(email: any): boolean;
-    }
+declare module medlemmer_common {
+    function getMedlemmerSheet():any;   
+    let INDMELDELSESDATO_COLUMNID:number;
+    let UDMELDT_COLUMN_ID:number;
+    let UDMELDELSES_COLUMNHEADER:string;
+    function getMedlemmerStamdata(): any;
+    function getMedlemmerSheet(): any;
+    function update(updateMedlem: any): void;
+    function addStamdataForMedlem(email: any, stamDataForMedlem: any): void;
+    function getNytMedlemsnummer(): any;
+    function erUdmeldt(email: any, stamdata: any): boolean;
+    function erMedlem(email: any, stamdata: any): boolean;
+    function getMedlemsNummmer(email: any, stamdata: any): any;
+    function harHylde(email: any): boolean;
+    function setUdmeldtStatus(email: any): void;
+    function erUdmeldt(email: any): boolean;
 }
