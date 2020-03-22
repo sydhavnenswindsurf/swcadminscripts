61db5e244987bc8aee6f297e3da554a0199e3a2e
Rasmus Lohals <lohals@gmail.com>
2020-03-22 13:02:23 +0100

    started new udmeldelser app

---
+++ udmeldelserapp/src/client/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Button from "@material-ui/core/Button";
+export const App = () => {
+  return (
+    <div>
+      hello XXX
+      <Button>Click t his</Button>
+    </div>
+  );
+};
