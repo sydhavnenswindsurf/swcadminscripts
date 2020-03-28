433733e653edb2c556141acd95ca6c29209e7bfd
Rasmus Lohals <lohals@gmail.com>
2020-03-28 22:19:45 +0100

    latest kontingentrapportapp

--- kontingentrapportapp/Code.js
+++ kontingentrapportapp/Code.js
@@ -1,204 +1,243 @@
 var _ = LodashGS.load();
-var MANUEL_REGISTRINGER_SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty("ManuelRegistreringerSheetId");
-var KONTINGENT_RAPPORT_TEMPLATE = "1nUDatf7SxoYOASDrVEeLjwGA2gtsYvS84RcwkaUcxDA";
-var CSV_HEADER_FORMAT = "\"Dato\";\"Tekst\";\"Bel\u00AFb\";\"Saldo\";\"Status\";\"Afstemt\"\r\n\"01.04.2016\";\"Jan Aasbjerg Peterse\";\"700,00\";\"435.039,07\";\"Udf\u00AFrt\";\"Nej\"";
+var MANUEL_REGISTRINGER_SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty(
+  "ManuelRegistreringerSheetId"
+);
+var KONTINGENT_RAPPORT_TEMPLATE =
+  "1nUDatf7SxoYOASDrVEeLjwGA2gtsYvS84RcwkaUcxDA";
+var CSV_HEADER_FORMAT =
+  '"Dato";"Tekst";"Bel\u00AFb";"Saldo";"Status";"Afstemt"\r\n"01.04.2016";"Jan Aasbjerg Peterse";"700,00";"435.039,07";"Udf\u00AFrt";"Nej"';
 var _RAPPORTS_FOLDER = "KontingentRapporter";
 function doGet() {
-    //return HtmlService.createHtmlOutput("<div>dsf</div>");
-    var html = HtmlService.createTemplateFromFile("main");
-    return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  //return HtmlService.createHtmlOutput("<div>dsf</div>");
+  var html = HtmlService.createTemplateFromFile("main");
+  return html
+    .evaluate()
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 function udmeld(emails) {
-    if (emails == null || emails.length == 0)
-        throw "ingen medlemmer angivet til udmeldelse";
-    var stamdata = medlemmer_common.getMedlemmerStamdata();
-    var result = [];
-    var i = 0;
-    for (i; i < emails.length; i++) {
-        var email = emails[i];
-        if (!medlemmer_common.erMedlem(email, stamdata)) {
-            throw email + " er ikke registreret medlem";
-        }
-        medlemmer_common.setUdmeldtStatus(email);
-        result.push({
-            email: email,
-            status: 'udmeldt'
-        });
+  if (emails == null || emails.length == 0)
+    throw "ingen medlemmer angivet til udmeldelse";
+  var stamdata = medlemmer_common.getMedlemmerStamdata();
+  var result = [];
+  var i = 0;
+  for (i; i < emails.length; i++) {
+    var email = emails[i];
+    if (!medlemmer_common.erMedlem(email, stamdata)) {
+      throw email + " er ikke registreret medlem";
     }
-    return result;
+    medlemmer_common.setUdmeldtStatus(email);
+    result.push({
+      email: email,
+      status: "udmeldt"
+    });
+  }
+  return result;
 }
 function searchForLastMailDate(emails) {
-    var returnList = [];
-    for (var i = 0; i < emails.length; i++) {
-        var returnObject = { lastActivity: null, email: emails[i] };
-        var result = GmailApp.search("from:" + emails[i]).map(function (thread) { return thread.getLastMessageDate(); });
-        if (result.length > 0) {
-            var sorted = result.sort(function (a, b) {
-                if (a < b)
-                    return 1;
-                if (a > b)
-                    return -1;
-                return 0;
-            });
-            returnObject.lastActivity = swcadmin_common.convertToStringsDate(sorted[0]);
-        }
-        returnList.push(returnObject);
+  var returnList = [];
+  for (var i = 0; i < emails.length; i++) {
+    var returnObject = { lastActivity: null, email: emails[i] };
+    var result = GmailApp.search("from:" + emails[i]).map(function(thread) {
+      return thread.getLastMessageDate();
+    });
+    if (result.length > 0) {
+      var sorted = result.sort(function(a, b) {
+        if (a < b) return 1;
+        if (a > b) return -1;
+        return 0;
+      });
+      returnObject.lastActivity = swcadmin_common.convertToStringsDate(
+        sorted[0]
+      );
     }
-    return returnList;
+    returnList.push(returnObject);
+  }
+  return returnList;
 }
 function getStats(id) {
-    var rapport = SpreadsheetApp.openById(id);
-    var rapportSheet = rapport.getSheetByName("Rapport");
-    var result = rapportSheet.getRange(2, 1, rapportSheet.getLastRow(), rapportSheet.getLastColumn()).getValues();
-    return result;
+  var rapport = SpreadsheetApp.openById(id);
+  var rapportSheet = rapport.getSheetByName("Rapport");
+  var result = rapportSheet
+    .getRange(2, 1, rapportSheet.getLastRow(), rapportSheet.getLastColumn())
+    .getValues();
+  return result;
 }
 function getLatestRapports() {
-    var currentYear = new Date().getFullYear();
-    var kontingentRapportFolders = DriveApp.getFoldersByName(_RAPPORTS_FOLDER);
-    var kontingentRapportFolder;
-    if (kontingentRapportFolders.hasNext()) {
-        kontingentRapportFolder = kontingentRapportFolders.next();
-    }
-    else {
-        throw "No kontingent rapport folder found";
-    }
-    var files = [];
-    var fileIterator = kontingentRapportFolder.getFiles();
-    while (fileIterator.hasNext()) {
-        files.push(fileIterator.next());
-    }
-    //sort by newest files, and return latest 5 and map to gui friendly object
-    var rapporter = _(files)
-        .sortBy(function (f) { return f.getLastUpdated(); })
-        .takeRight(5)
-        .value()
-        .map(function (f) {
-        return {
-            url: f.getUrl(),
-            name: f.getName(),
-            id: f.getId(),
-            date: swcadmin_common.convertToStringsDate(f.getLastUpdated())
-        };
+  var currentYear = new Date().getFullYear();
+  var kontingentRapportFolders = DriveApp.getFoldersByName(_RAPPORTS_FOLDER);
+  var kontingentRapportFolder;
+  if (kontingentRapportFolders.hasNext()) {
+    kontingentRapportFolder = kontingentRapportFolders.next();
+  } else {
+    throw "No kontingent rapport folder found";
+  }
+  var files = [];
+  var fileIterator = kontingentRapportFolder.getFiles();
+  while (fileIterator.hasNext()) {
+    files.push(fileIterator.next());
+  }
+  //sort by newest files, and return latest 5 and map to gui friendly object
+  var rapporter = _(files)
+    .sortBy(function(f) {
+      return f.getLastUpdated();
+    })
+    .takeRight(5)
+    .value()
+    .map(function(f) {
+      return {
+        url: f.getUrl(),
+        name: f.getName(),
+        id: f.getId(),
+        date: swcadmin_common.convertToStringsDate(f.getLastUpdated())
+      };
     });
-    return rapporter;
+  return rapporter;
 }
 function createNewRapport(formObject) {
-    if (!_isValidCsvFormat(formObject.csv)) {
-        throw "Det forventes at konto udtog filen er en semi kolon separeret .csv  fil med eksempel formatet: \n\n" +
-            CSV_HEADER_FORMAT;
-    }
-    var kontoData = _parseCsv(formObject.csv);
-    var newRapportId = _cloneRapportTemplate();
-    var newRapport = SpreadsheetApp.openById(newRapportId);
-    _insertKontoData(kontoData, newRapport);
-    var medlemmer = medlemmer_common.getMedlemmerSheet();
-    _copyIndmeldteData(newRapport, medlemmer);
-    _addManualIndbetalinger(newRapport);
-    _setCalculationForAllMembers(newRapport);
-    newRapport.setActiveSheet(newRapport.getSheetByName("Rapport"));
-    return { url: newRapport.getUrl(), name: newRapport.getName(), id: newRapport.getId(), date: swcadmin_common.convertToStringsDate(new Date()) };
+  if (!_isValidCsvFormat(formObject.csv)) {
+    throw "Det forventes at konto udtog filen er en semi kolon separeret .csv  fil med eksempel formatet: \n\n" +
+      CSV_HEADER_FORMAT;
+  }
+  var kontoData = _parseCsv(formObject.csv);
+  var newRapportId = _cloneRapportTemplate();
+  var newRapport = SpreadsheetApp.openById(newRapportId);
+  _insertKontoData(kontoData, newRapport);
+  var medlemmer = medlemmer_common.getMedlemmerSheet();
+  _copyIndmeldteData(newRapport, medlemmer);
+  _addManualIndbetalinger(newRapport);
+  _setCalculationForAllMembers(newRapport);
+  newRapport.setActiveSheet(newRapport.getSheetByName("Rapport"));
+  return {
+    url: newRapport.getUrl(),
+    name: newRapport.getName(),
+    id: newRapport.getId(),
+    date: swcadmin_common.convertToStringsDate(new Date())
+  };
 }
 function _addManualIndbetalinger(newRapport) {
-    var sheet = SpreadsheetApp.openById(MANUEL_REGISTRINGER_SPREADSHEET_ID).getSheetByName("emails");
-    var data = sheet.getRange(1, 1, sheet.getLastRow(), 1).getValues();
-    var targetSheet = newRapport.getSheetByName("Manuel registrering");
-    if (data.length > 0) {
-        targetSheet.getRange(2, 1, data.length, data[0].length).setValues(data);
-    }
-    var calculationSource = targetSheet.getRange(2, 6, 1, 1);
-    calculationSource.copyTo(targetSheet.getRange(3, 6, data.length, 1));
+  var sheet = SpreadsheetApp.openById(
+    MANUEL_REGISTRINGER_SPREADSHEET_ID
+  ).getSheetByName("emails");
+  var data = sheet.getRange(1, 1, sheet.getLastRow(), 1).getValues();
+  var targetSheet = newRapport.getSheetByName("Manuel registrering");
+  if (data.length > 0) {
+    targetSheet.getRange(2, 1, data.length, data[0].length).setValues(data);
+  }
+  var calculationSource = targetSheet.getRange(2, 6, 1, 1);
+  calculationSource.copyTo(targetSheet.getRange(3, 6, data.length, 1));
 }
 function _setCalculationForAllMembers(newRapport) {
-    var rapportSheet = newRapport.getSheetByName("Rapport");
-    //  
-    //  sheet.insertRows(3, medlemmer.getLastRow()-2);
-    //  
-    var calculationRange = rapportSheet.getRange(2, 1, 1, rapportSheet.getLastColumn());
-    //  
-    var newRapportMedlemsliste = newRapport.getSheetByName("Medlemsliste");
-    var lastCol = calculationRange.getLastColumn();
-    calculationRange.copyTo(rapportSheet.getRange(3, 1, newRapportMedlemsliste.getLastRow() - 2, lastCol));
+  var rapportSheet = newRapport.getSheetByName("Rapport");
+  //
+  //  sheet.insertRows(3, medlemmer.getLastRow()-2);
+  //
+  var calculationRange = rapportSheet.getRange(
+    2,
+    1,
+    1,
+    rapportSheet.getLastColumn()
+  );
+  //
+  var newRapportMedlemsliste = newRapport.getSheetByName("Medlemsliste");
+  var lastCol = calculationRange.getLastColumn();
+  calculationRange.copyTo(
+    rapportSheet.getRange(
+      3,
+      1,
+      newRapportMedlemsliste.getLastRow() - 2,
+      lastCol
+    )
+  );
 }
 function _isValidCsvFormat(file) {
-    var headerData = file.getBlob()
-        .getDataAsString()
-        .split("\n")[0]
-        .split(";");
-    var result = headerData[0].match(/^"?Dato"?$/) != null
-        && headerData[1].match(/^"?Tekst"?$/) != null
-        && headerData[2].match(/^"?Bel\Sb"?$/) != null;
-    ;
-    return result;
+  var headerData = file
+    .getBlob()
+    .getDataAsString()
+    .split("\n")[0]
+    .split(";");
+  var result =
+    headerData[0].match(/^"?Dato"?$/) != null &&
+    headerData[1].match(/^"?Tekst"?$/) != null &&
+    headerData[2].match(/^"?Bel\Sb"?$/) != null;
+  return result;
 }
 function _parseCsv(file) {
-    var result = file.getBlob()
-        .getDataAsString()
-        .split("\n")
-        .map(function (row) {
-        var row = row.split(";")
-            .map(function (row) {
-            return row.replace(/^\"/, "").replace(/\"$/, "");
-        });
-        row[2] = parseInt(row[2]);
-        return row;
+  var result = file
+    .getBlob()
+    .getDataAsString()
+    .split("\n")
+    .map(function(row) {
+      var row = row.split(";").map(function(row) {
+        return row.replace(/^\"/, "").replace(/\"$/, "");
+      });
+      row[2] = parseInt(row[2]);
+      return row;
     });
-    return result;
+  return result;
 }
 function _cloneRapportTemplate() {
-    var rapportFolders = DriveApp.getFoldersByName(_RAPPORTS_FOLDER);
-    var rapportFolder;
-    if (rapportFolders.hasNext()) {
-        rapportFolder = rapportFolders.next();
-    }
-    else {
-        rapportFolder = DriveApp.createFolder(_RAPPORTS_FOLDER);
-    }
-    return DriveApp
-        .getFileById(KONTINGENT_RAPPORT_TEMPLATE)
-        .makeCopy("Kontingent opgørelse " + new Date(), rapportFolder).getId();
+  var rapportFolders = DriveApp.getFoldersByName(_RAPPORTS_FOLDER);
+  var rapportFolder;
+  if (rapportFolders.hasNext()) {
+    rapportFolder = rapportFolders.next();
+  } else {
+    rapportFolder = DriveApp.createFolder(_RAPPORTS_FOLDER);
+  }
+  return DriveApp.getFileById(KONTINGENT_RAPPORT_TEMPLATE)
+    .makeCopy("Kontingent opgørelse " + new Date(), rapportFolder)
+    .getId();
 }
 function _copyIndmeldteData(newRapport, medlemmer) {
-    var currentYear = new Date().getFullYear();
-    var targetSheet = newRapport.getSheetByName("Medlemsliste").clear();
-    var valuesToCopy = medlemmer
-        .getRange(1, 1, medlemmer.getLastRow(), medlemmer.getLastColumn())
-        .getValues()
-        .filter(function (row, index) {
-        //filter out members indmeldt in current year
-        var indmeldDate = row[medlemmer_common.INDMELDELSESDATO_COLUMNID - 1];
-        if (indmeldDate != null && indmeldDate.getFullYear !== undefined && currentYear === indmeldDate.getFullYear())
-            return false;
-        var udmeldtValue = row[medlemmer_common.UDMELDT_COLUMN_ID - 1];
-        return (udmeldtValue == '' || udmeldtValue == medlemmer_common.UDMELDELSES_COLUMNHEADER);
+  var currentYear = new Date().getFullYear();
+  var targetSheet = newRapport.getSheetByName("Medlemsliste").clear();
+  var valuesToCopy = medlemmer
+    .getRange(1, 1, medlemmer.getLastRow(), medlemmer.getLastColumn())
+    .getValues()
+    .filter(function(row, index) {
+      //filter out members indmeldt in current year
+      var indmeldDate = row[medlemmer_common.INDMELDELSESDATO_COLUMNID - 1];
+      if (
+        indmeldDate != null &&
+        indmeldDate.getFullYear !== undefined &&
+        currentYear === indmeldDate.getFullYear()
+      )
+        return false;
+      var udmeldtValue = row[medlemmer_common.UDMELDT_COLUMN_ID - 1];
+      return (
+        udmeldtValue == "" ||
+        udmeldtValue == medlemmer_common.UDMELDELSES_COLUMNHEADER
+      );
     });
-    targetSheet.getRange(2, 1, valuesToCopy.length, valuesToCopy[0].length).setValues(valuesToCopy);
+  targetSheet
+    .getRange(2, 1, valuesToCopy.length, valuesToCopy[0].length)
+    .setValues(valuesToCopy);
 }
 function _insertKontoData(kontoData, newRapport) {
-    var sheet = newRapport
-        .getSheetByName("Konto udtog")
-        .clear();
-    sheet.getRange(1, 1, kontoData.length, kontoData[0].length).setValues(kontoData);
+  var sheet = newRapport.getSheetByName("Konto udtog").clear();
+  sheet
+    .getRange(1, 1, kontoData.length, kontoData[0].length)
+    .setValues(kontoData);
 }
 function test_searchForLastMailDate(email) {
-    var result = searchForLastMailDate("xxx@gmail.com");
-    alert(result);
+  var result = searchForLastMailDate("xxx@gmail.com");
+  alert(result);
 }
 function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename)
-        .getContent();
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 function testcreateNewRapport() {
-    var testfile = DriveApp.getFileById("0B6zVGnjYaDZiQ1NMYmdkbS1Id28");
-    //var data = testfile.getBlob().getDataAsString().split("\n");
-    var result = createNewRapport({
-        csv: testfile
-    });
+  var testfile = DriveApp.getFileById("0B6zVGnjYaDZiQ1NMYmdkbS1Id28");
+  //var data = testfile.getBlob().getDataAsString().split("\n");
+  var result = createNewRapport({
+    csv: testfile
+  });
 }
 function testGetStats() {
-    var result = getStats("1eAFkk-YGDBL8DjpLAGnKrioVpFkNWH5toNZi442qtNA");
-    var x = 2;
+  var result = getStats("1eAFkk-YGDBL8DjpLAGnKrioVpFkNWH5toNZi442qtNA");
+  var x = 2;
 }
 function testUdmeld() {
-    var result = udmeld(["xx@gmail.com"]);
-    console.log(result);
+  var result = udmeld(["xx@gmail.com"]);
+  console.log(result);
 }
